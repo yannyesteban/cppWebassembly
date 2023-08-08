055e0635ba37c71903244fb70794cac0c9da69f6
Debian <yannyesteban@gmail.com>
2023-08-08 15:59:59 -0400

    ok

---
+++ wa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <emscripten/emscripten.h>
+
+int main() {
+    printf("Hello World 3\n");
+    return 0;
+}
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+#else
+#define EXTERN
+#endif
+
+EXTERN EMSCRIPTEN_KEEPALIVE void myFunction(int argc, char ** argv) {
+    printf("MyFunction Called\n");
+}
+
+
+EXTERN EMSCRIPTEN_KEEPALIVE int suma(int a, int b) {
+    return a+b;
+}
